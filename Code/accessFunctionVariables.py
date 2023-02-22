821db1b605906e672112786c590c437901557066
Aspenaz <aspenaz@users.noreply.github.com>
2023-02-22 18:34:42 -0500

    Completed print() method

--- Code/accessFunctionVariables.py
+++ Code/accessFunctionVariables.py
@@ -1,5 +1,5 @@
 # Example:
-
+""" 
 def xxx():
         a=10
         b=15
@@ -13,18 +13,18 @@ def yyy():
 yyy()
 
 
-
-
+ """
+print()
 # ===========================================================
 # Answers 1:
 
-def xxx():
+def xxx():   # type: ignore
     a=10
     b=15
     c=20
     return a, b
 
-def yyy():
+def yyy():   # type: ignore
     a, b = xxx()
     print(a)        ### value a from xxx()
     print(b)        ### value b from xxx()
@@ -35,16 +35,19 @@ yyy()
 
 
 
+print()
 # ===========================================================
+
 # Answers 2:
 
-def xxx():
+print('global 2\n') 
+def xxx():   # type: ignore
     global a, b
-    a=10
-    b=15
-    c=20
+    a=110   # type: ignore
+    b=115   # type: ignore
+    c=220
 
-def yyy():
+def yyy():   # type: ignore
     print (a)
     print (b)
 
@@ -54,68 +57,124 @@ yyy()
 # 15
 
 
+print()
 # ===========================================================
+
 # Answers 3:
-""" 
-global a=0
-global b=0
-global c=0
+
+print('global 3\n')
+global a, b, c
+# global b
+# global c 
+
+a = 0   # type: ignore
+b = 0   # type: ignore
+c = 0
 
 def xxx():
-    a=10
-    b=15
-    c=20
-    return a,b
+    a=101
+    b=151
+    c=202
+    return a, b
 
 def yyy():
-    print (a)
-    print (b)
+    print ('a=', a)
+    print ('b=', b)
     xxx()
 
- """
+print(yyy())
+print('xxx=', xxx())
+yyy()
+ 
+
+ 
+print()
 # ===========================================================
+
 # Answers 4:
 
+print('MyClass 1\n')
+class MyClass(object):   # type: ignore
+    x = [1, 2, 3]
+    def xxx(self):
+        self.a = 100
+        self.b = 200
+        self.c = 300
+
+    def yyy(self):
+        self.xxx()
+        self.d = 400
+        print (self.a)
+        print (self.b)
+
+obj = MyClass()  # Create an instance of the class
+obj.yyy()
+
+print()
+print(obj.a) 
+print(obj.b) 
+print(obj.c)  # 200
+print(obj.d)  # 300
+
+
+
+print()
+# ===========================================================
+
+
+print('MyClass 2 \n')
 class MyClass(object):
     x = [1, 2, 3]
     def xxx(self):
         self.a = 100
-        self.b = 150
-        self.c = 200
+        self.b = 200
+        self.c = 300
 
     def yyy(self):
         self.xxx()
-        self.d = 300
-        print (self.a)
-        print (self.b)
+        self.d = 400
+        # print ('a =', self.a)
+        # print ('b =', self.b)
+        self.e = self.a
+        self.f = self.b
 
+obj = MyClass()  # Create an instance of the class
+obj.yyy()  # 100  150
 
-obj = MyClass()  #Create an instance of the class
-obj.yyy()
-# 100
-# 150
-print()
-print(obj.c)  # 200
-print(obj.d)  # 300
+# obj.xxx()  # If not execute xxx() method don't start and initialize variables a, b, c. 
+print('a=', obj.a)  # 200
+print('b=', obj.b)  # 300
+print('c=', obj.c)  # 200
+print('d=', obj.d)  # 300  # AttributeError: 'MyClass' object has no attribute 'd'. # In this case if not execute yyy() method.
+print('e=', obj.e)  # 200
+print('f=', obj.f)  # 300
 # print(obj.xxx.get.x)  # Extract innner scope variable!!!!!!!! How is it? 
 # print(MyClass.c)
 
 
 
+print()
 # ===========================================================
+#
 # ===========================================================
 
 
-def a(): 
+def a():   # type: ignore 
 	a.i="variable1"  # function attribute  
 	 
-def b(): 
+def b():   # type: ignore 
 	a()         # call the function  
 	print(a.i)  # acess the variable  
 	 
 b()  # variable1
 
+
+
+print()
 # ===========================================================
+
+
+
 
 def a(): 
 	global i 
@@ -128,4 +187,8 @@ def b():
 b()  # variable2
 
 
+
+
+print()
+# ===========================================================
 
--- Code/tkinter/tk-003.py
+++ Code/tkinter/tk-003.py
@@ -1,20 +1,18 @@
-""" Ventanas de aplicación y de diálogos 
+""" Ventanas de aplicación y de diálogo: 
 
-Ventanas de aplicación se definen con Tk():
-self.raiz = Tk()
+Ventanas de aplicación se definen con Tk(): self.raiz = Tk()
 
-Ventanas de diálogo (hijas) se definen con Toplevel():
-self.dialogo = Toplevel() 
+Ventanas de diálogo (hijas) se definen con Toplevel(): self.dialogo = Toplevel() 
 
 
 
-mainloop() (bucle principal) atiende eventos de la ventana de aplicación:
-self.raiz.mainloop()
+mainloop() (bucle principal) 
+Atiende eventos de la ventana de aplicación: self.raiz.mainloop()
 
-wait_window() atiende eventos de la ventana de diálogo mientras espera a ser cerrada:
-self.raiz.wait_window(self.dialogo) """
+wait_window() 
+Atiende eventos de la ventana de diálogo mientras espera a ser cerrada: self.raiz.wait_window(self.dialogo) 
+"""
 
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from tkinter import *
@@ -32,27 +30,25 @@ class Aplicacion():
         self.raiz = Tk()
         self.raiz.geometry('347x216+500+50') 
         self.raiz.title("Ventana de aplicación")
-        self.raiz.resizable(0,0)   
-
+        self.raiz.resizable(0, 0) # type: ignore
 
         scrollbar = Scrollbar(self.raiz)  
-        scrollbar.grid(row=0, column=2, padx=.1, sticky=(N, S)) # 
-
+        scrollbar.grid(row=0, column=2, padx=.1, sticky=(N, S)) # type: ignore #
 
         self.text_info = Text (self.raiz,  width=40, height=10, bg='beige' , yscrollcommand=scrollbar.set) 
-        self.text_info.grid(row=0, column=0, columnspan=2, padx=2, pady=2, sticky=(E, W)) 
-
-
-        scrollbar.config(command=self.text_info.yview)  
-
-        separador_1 = ttk.Separator(self.raiz, orient=HORIZONTAL)   
-        boton_abrir  = ttk.Button(self.raiz, text='Abrir', command=self.abrir )     
-        boton_cerrar = ttk.Button(self.raiz, text='Cerrar', command=self.raiz.destroy)    
-             
-        separador_1.grid(row=1, column=0, columnspan=2, padx=5, pady=2, sticky=(E, W))
-        boton_abrir.grid(row=2, column=0, padx=20, pady=6)  
-        boton_cerrar.grid(row=2, column=1, padx=20, pady=6)  
+        self.text_info.grid(row=0, column=0, columnspan=2, padx=2, pady=2, sticky=(E, W))  # type: ignore
         
+        scrollbar.config(command=self.text_info.yview)  
+        
+        separador_1 = ttk.Separator(self.raiz, orient=HORIZONTAL)  
+        separador_1.grid(row=1, column=0, columnspan=2, padx=5, pady=2, sticky=(E, W))  # type: ignore 
+        
+        boton_abrir  = ttk.Button(self.raiz, text='Abrir', command=self.abrir)  
+        boton_abrir.grid(row=2, column=0, padx=20, pady=6)  
+           
+        boton_cerrar = ttk.Button(self.raiz, text='Cerrar', command=self.raiz.destroy) 
+        boton_cerrar.grid(row=2, column=1, padx=20, pady=6)   
+            
 
         boton_abrir.focus_set()
 
@@ -61,7 +57,6 @@ class Aplicacion():
 
     def abrir(self):
         ''' Construye una ventana de diálogo '''
-
         self.dialogo = Toplevel()
 
         Aplicacion.ventana+=1
@@ -70,10 +65,11 @@ class Aplicacion():
         self.tamypos = '250x100+' + str(Aplicacion.posx_y) + '+' + str(Aplicacion.posx_y)
         
         self.ident = self.dialogo.winfo_id()  
+        
         titulo = str(Aplicacion.ventana) + ": " + str(self.ident)
 
         self.dialogo.geometry(self.tamypos)
-        self.dialogo.resizable(0,0)
+        self.dialogo.resizable(0, 0)  # type: ignore
         self.dialogo.title(titulo)
 
         boton_cerrar_2 = ttk.Button(self.dialogo, text='Cerrar', command=self.dialogo.destroy)
@@ -82,84 +78,63 @@ class Aplicacion():
 
         self.ver_info()
 
-        self.print()
-
         self.raiz.wait_window(self.dialogo)
+    
+    
+    def ver_info(self):
+        
+        self.print()
+        
+        # self.text_info.delete("1.0", END)
 
-  
+        text_in = '\n'
+        text_in += 'Ventana: '          + str(Aplicacion.ventana)   + '\n'      #todo 
+        text_in += 'Id: '               + str(self.ident)           + '\n'      #todo 
+        # text_in += 'Class: '            + self._class_dialogo       + '\n'   
+        text_in += 'Geometria: '        + self.tamypos              + '\n'      #todo      
+        # text_in += 'Anchura ventana: '+ str(self._width_dialogo)    + '\n'
+        # text_in += 'Posx_y: '         + str(Aplicacion.posx_y)      + '\n' 
+        # text_in += 'Altura ventana: ' + str(self._height_dialogo)   + '\n' 
+        text_in += 'Nombre: '           + self._name_dialogo        + '\n'      #todo 
+
+        self.text_info.insert('1.0', text_in) 
+           
+        
+
     def print(self):
-        self._class_dialogo  = self.dialogo.winfo_class()
-        print()
-        print('_class_dialogo(in print): ', self._class_dialogo)
-        # self._geometry_dialogo = self.dialogo.winfo_geometry()
-        # self._width_dialogo  = self.dialogo.winfo_width()
-        # self._height_dialogo = self.dialogo.winfo_height()
-        # self._rootx_dialogo = self.dialogo.winfo_rootx()
-        # self._rooty_dialogo = self.dialogo.winfo_rooty()
-        # self.ident = self.dialogo.winfo_id()  # self.identicador de la nueva ventana
-        # self._name_dialogo = self.dialogo.winfo_name()
-        # self._manager_dialogo  = self.dialogo.winfo_manager()
-        # self._vrootx_dialogo = self.dialogo.winfo_vrootx()
-        # self._vrooty_dialogo = self.dialogo.winfo_vrooty()
-        # self._server_dialogo = self.dialogo.winfo_server()
-        # self._toplevel_dialogo = self.dialogo.winfo_toplevel()
-        # self._screenvisual_dialogo = self.dialogo.winfo_screenvisual()   
+        self._class_dialogo         = self.dialogo.winfo_class()        #todo
+        self._geometry_dialogo      = self.dialogo.winfo_geometry()     #todo
+        self._width_dialogo         = self.dialogo.winfo_width()
+        self._height_dialogo        = self.dialogo.winfo_height()
+        self._rootx_dialogo         = self.dialogo.winfo_rootx()
+        self._rooty_dialogo         = self.dialogo.winfo_rooty()
+        self._name_dialogo          = self.dialogo.winfo_name()         #todo
+        self._manager_dialogo       = self.dialogo.winfo_manager()
+        self._vrootx_dialogo        = self.dialogo.winfo_vrootx()
+        self._vrooty_dialogo        = self.dialogo.winfo_vrooty()
+        self._server_dialogo        = self.dialogo.winfo_server()
+        self._toplevel_dialogo      = self.dialogo.winfo_toplevel()
+        self._screenvisual_dialogo  = self.dialogo.winfo_screenvisual()   
 
-        # print()
         print('ventana:', Aplicacion.ventana)
         print('posx_y:', Aplicacion.posx_y)        
         print('tamypos:', self.tamypos)
         print('ident:', self.ident)
-        # print('class:', self._print._class_dialogo)
-        # print('geometry:', self._geometry_dialogo)
-        # print('width:', self._width_dialogo)
-        # print('height:', self._height_dialogo)
-        # print('rootx:', self._rootx_dialogo)
-        # print('rooty:', self._rooty_dialogo)
-        # print('name:', self._name_dialogo)
-        # print('manager:', self._manager_dialogo)
-        # print('vrootx:', self._vrootx_dialogo)
-        # print('vrooty:', self._vrooty_dialogo)
-        # print('server:', self._server_dialogo)
-        # print('toplevel:', self._toplevel_dialogo)
-        # print('screenvisual:', self._screenvisual_dialogo)
-
-
-    def ver_info(self):
-        # self.text_info.delete("1.0", END)
-
-        info1 = str(Aplicacion.ventana)  
-        info2 = str(self.ident)
-        # info3 = self._class_dialogo
-        # info4 = self._name_dialogo
-        info5 = self.tamypos 
-        # info6 = str(self._width_dialogo)    
-        # info7 = str(Aplicacion.posx_y)   
-        # info8 = str(self._height_dialogo) 
-
-        text_in = '\n'
-        text_in += 'Ventana: ' + info1 + '\n'
-        text_in += 'Id: ' + info2 + '\n' 
-        # text_in += 'Class: ' + info3 + '\n'  
-        # text_in += 'Name: ' + info4 + '\n'  
-        text_in += 'Geometria: ' + info5 + '\n'        
-        # # text_in += 'Anchura ventana: ' + info6 + '\n'
-        # text_in += 'Posx_y: ' + info7 + '\n' 
-        # # text_in += 'Altura ventana: ' + info8 + '\n' 
-
+        
+        print('class:', self._class_dialogo)                    #todo      # Toplevel
+        print('geometry:', self._geometry_dialogo)              #todo
+        print('width:', self._width_dialogo)
+        print('height:', self._height_dialogo)
+        print('rootx:', self._rootx_dialogo)
+        print('rooty:', self._rooty_dialogo)
+        print('name:', self._name_dialogo)                      #todo
+        print('manager:', self._manager_dialogo)
+        print('vrootx:', self._vrootx_dialogo)
+        print('vrooty:', self._vrooty_dialogo)
+        print('server:', self._server_dialogo)
+        print('toplevel:', self._toplevel_dialogo)
+        print('screenvisual:', self._screenvisual_dialogo)
         print()
-        print('_class_dialogo(in ver_info): ', self._class_dialogo)
-        # print(text_in)
-        # print('ventana:', Aplicacion.ventana)
-        # print('posx_y:', Aplicacion.posx_y)        
-        # print('tamypos:', tamypos)
-        # print('self.ident:', self.ident)
-        # print()
-
-        self.text_info.insert('1.0', text_in)
-
-
-
 
 
 def main():
@@ -168,4 +143,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
